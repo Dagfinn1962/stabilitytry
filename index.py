1fb0e2ae32d1b9700059a7c61764115f71096813
Dagfinn1962 <130903243+Dagfinn1962@users.noreply.github.com>
2023-05-23 20:29:05 +0200

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
 
 os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
-os.environ['STABILITY_KEY'] = 'API KEY HERE '
+os.environ['STABILITY_KEY'] = 'sk-Vf1g2hV9pvGAJYm5n16EYB7D7veCInOd13RXBit99qRFlFDd'
 stability_api = client.StabilityInference(key=os.environ['STABILITY_KEY'],
 verbose=True,
 engine="stable-diffusion-xl-beta-v2-2-2",
@@ -23,3 +23,4 @@ width=512,
 height=512, 
 samples=1, 
 sampler=generation.SAMPLER_K_DPMPP_2M )
+
